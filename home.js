caa70d6fe3a2277bf10efbead28a4dfc8f88afc2
ammar <ammaraptech007@gmail.com>
2023-12-11 01:57:08 +0500

    do all files commit

--- home.js
+++ home.js
@@ -1192,3 +1192,33 @@ $(document).ready(function() {
   };
 }(jQuery));
 // top //
+
+// review //
+$('.clients-carousel').owlCarousel({
+    loop: true,
+    nav: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+    smartSpeed: 450,
+    margin: 30,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        991: {
+            items: 2
+        },
+        1200: {
+            items: 2
+        },
+        1920: {
+            items: 2
+        }
+    }
+});
+// review //
